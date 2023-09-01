c840fa4fc78a63fb7275d0f91d1feb95ecf2451a
vagabond <athomec@pie.com.tw>
2023-09-01 14:39:11 +0800

    選單調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(function () {//JS開頭
 
 	var WINDOW = $(window).width();//視窗寬度
 	var WINDOWH = $(window).height();//視窗高度
-	
+
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 50) { //若目前的位置距離網頁頂端>...
 			$(".gotop").addClass("show");
@@ -16,7 +16,28 @@ $(function () {//JS開頭
 	//字級大小
 	$(".js-toggle-btn").click(function () {
 		$(this).toggleClass("active");
-	})
+	});
 
-	
+	//菜單斷行判斷
+	function checkLineBreaks() {
+		var paragraphContainers = document.querySelectorAll('.js-theme-list-item-body');
+		paragraphContainers.forEach(function (container) {
+			var paragraph = container.querySelector('p');
+			var paragraphHeight = paragraph.clientHeight;
+			var contentHeight = paragraph.scrollHeight;
+
+			if (contentHeight > paragraphHeight) {
+				container.classList.add("hasbreaks");
+			} else {
+				container.classList.remove("hasbreaks");
+			}
+		});
+	}
+
+	checkLineBreaks();
+
+	window.addEventListener('resize', function () {
+		checkLineBreaks();
+	});
+
 })//JS尾端	
