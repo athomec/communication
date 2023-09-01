50ef563b6e22e06a02fc4acf3bc081ff17d0e554
vagabond <athomec@pie.com.tw>
2023-09-01 17:17:53 +0800

    選單與跑馬燈修改

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,4 +40,35 @@ $(function () {//JS開頭
 		checkLineBreaks();
 	});
 
+	//滑鼠移入下拉選單
+
+	var dropdownToggles = document.querySelectorAll('.js-dropdown-toggle');
+	var dropdown = document.querySelectorAll('.js-dropdown');
+
+	// 遍历这些元素，为它们添加事件监听器
+	dropdownToggles.forEach(function (toggle) {
+		// 监听鼠标悬停事件
+		toggle.addEventListener('mouseenter', function () {
+			// 创建并模拟一个鼠标点击事件
+			var clickEvent = new MouseEvent('click', {
+				bubbles: true,
+				cancelable: true,
+				view: window
+			});
+			toggle.dispatchEvent(clickEvent); // 触发点击事件
+		});
+
+	});
+	dropdown.forEach(function (toggle) {
+		// 监听鼠标移出事件
+		toggle.addEventListener('mouseleave', function () {
+			// 隐藏下拉菜单
+			var clickEvent = new MouseEvent('click', {
+				bubbles: true,
+				cancelable: true,
+				view: window
+			});
+			toggle.dispatchEvent(clickEvent); // 触发点击事件
+		});
+	});
 })//JS尾端	
